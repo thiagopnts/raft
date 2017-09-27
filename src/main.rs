0a0f5e0c097e621d8c5dc6201a45d980bd91faa2
Thiago Pontes <github@thiago.me>
2017-09-27 19:42:28 -0400

    add basic decode example. decoding is done in place so bencode order matters

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,67 @@
 extern crate bencode;
 
 use bencode::{Decodable, Decoder, DecodeError};
+use std::fs::File;
+use std::io::{self, Read, Write};
 
-enum Info {
-    Single,
-    Multiple,
+struct SingleFile {
+    length: i64,
+    name: String,
+    piece_length: usize,
+    //pieces: Vec<u8>,
 }
-
-impl Decodable for Info {
-    fn decode(_: &mut Decoder) -> Result<Info, DecodeError> {
-        Ok(Info::Single)
+impl Decodable for SingleFile {
+    fn decode(d: &mut Decoder) -> Result<SingleFile, DecodeError> {
+        d.read_struct(|d| {
+            Ok(SingleFile{
+                length: try!(d.read_field("length")),
+                name: try!(d.read_field("name")),
+                piece_length: try!(d.read_field("piece length")),
+                //pieces: try!(d.read_field("pieces")),
+            })
+        })
     }
 }
 
 struct MetaInfo {
-    info: Info,
+    pub announce: String,
+    pub created_by: String,
+    pub comment: String,
+    pub encoding: String,
+    info: SingleFile,
+}
+
+fn read_string_with_default(d: &mut Decoder, field_name: &'static str, default: &'static str) -> String {
+    match d.read_field(field_name) {
+        Ok(field) => field,
+        Err(_) => default.to_string(),
+    }
 }
 
 impl Decodable for MetaInfo {
     fn decode(d: &mut Decoder) -> Result<MetaInfo, DecodeError> {
         d.read_struct(|d| {
             Ok(MetaInfo{
-                info: try!(d.read_field("info"))
+                announce: try!(d.read_field("announce")),
+                comment: try!(d.read_field("comment")),
+                created_by: read_string_with_default(d, "created by", ""),
+                encoding: try!(d.read_field("encoding")),
+                info: try!(d.read_field("info")),
             })
         })
     }
 }
 
 fn main() {
-    println!("hello");
+    let mut file = File::open("harry.torrent").expect("error opening file");
+    let mut buf = Vec::new();
+    let _ = file.read_to_end(&mut buf);
+    let meta_info = bencode::decode::<MetaInfo>(&buf).unwrap();
+    println!("GET {}", meta_info.announce);
+    println!("name {}", meta_info.info.name);
+    println!("total len {}", meta_info.info.length);
+    println!("piece len {}", meta_info.info.piece_length);
+    println!("created by {}", meta_info.created_by);
+    println!("comment {}", meta_info.comment);
+    println!("encoding {}", meta_info.encoding);
 }
